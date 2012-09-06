c3a439d42dfa67710f5afcc79ce267cba776969d
Henrik Singmann <henrik.singmann (at) psychologie.uni-freiburg.de>
2012-09-06 16:41:52 +0200

    added elements and better import statements

--- tryout/psytml1.py
+++ tryout/psytml1.py
@@ -1,33 +1,32 @@
 import sys
 from urllib import unquote_plus
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyQt4.QtWebKit import *
+from PyQt4 import QtCore, QtGui, QtWebKit
 
-class MyWebPage(QWebPage):
-    formSubmitted = pyqtSignal(QUrl)
+class MyWebPage(QtWebKit.QWebPage):
+    formSubmitted = QtCore.pyqtSignal(QtCore.QUrl)
 
     def acceptNavigationRequest(self, frame, req, nav_type):
-        if nav_type == QWebPage.NavigationTypeFormSubmitted:
+        if nav_type == QtWebKit.QWebPage.NavigationTypeFormSubmitted:
             self.formSubmitted.emit(req.url())
         return super(MyWebPage, self).acceptNavigationRequest(frame, req, nav_type)
 
-class Window(QWidget):
-    def __init__(self, html):
+class Window(QtGui.QWidget):
+    def __init__(self, html, elements):
         super(Window, self).__init__()
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        view = QWebView(self)
-        layout = QVBoxLayout(self)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        view = QtWebKit.QWebView(self)
+        layout = QtGui.QVBoxLayout(self)
         layout.addWidget(view)
         layout.setContentsMargins(0, 0, 0, 0)
         view.setPage(MyWebPage())
         view.setHtml(html)
         view.page().formSubmitted.connect(self.handleFormSubmitted)
+        self.elements = elements
 
     def handleFormSubmitted(self, url):
+        elements = self.elements
         self.close()
-        elements = {}
         for key, value in url.encodedQueryItems():
             key = unquote_plus(bytes(key)).decode('utf8')
             value = unquote_plus(bytes(value)).decode('utf8')
@@ -35,7 +34,7 @@ class Window(QWidget):
         # do stuff with elements...
         for item in elements.iteritems():
             print '"%s" = "%s"' % item
-        qApp.quit()
+        QtGui.qApp.quit()
 
 # setup the html form
 html = """
@@ -49,11 +48,12 @@ Like it?
 """
 
 def main():
-    app = QApplication(sys.argv)
-
-    window = Window(html)
+    f1 = {}
+    app = QtGui.QApplication(sys.argv)
+    window = Window(html, f1)
     window.show()
     app.exec_()
+    print(f1)
 
 if __name__ == "__main__":
     main()
