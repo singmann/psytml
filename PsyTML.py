916a669228a556823d0f6e9b65fccf2ece6b9be9
Konstantin Sering <konstantin.sering@gmail.com>
2012-09-20 16:45:05 +0100

    changed positions default in PsyTML from True to None; deleted dublicated files; added some docstrings; added file in examples/psychopy/data/ to ensure this folder will be created by git; improved code readability by breaking lines after 78 characters

--- PsyTML.py
+++ PsyTML.py
@@ -1,12 +1,26 @@
 # version 1.0: 7th September 2012
 # Henrik Singmann with help from ekhumoror (http://stackoverflow.com/users/984421/ekhumoro)
 # License: GPL v3
+# last mod 2012-09-20 13:42 KS
+
+"""
+psytml provides a convenient way to present a html form at the screen and get
+the form's data into python.
+
+"""
 
 from urllib import unquote_plus
 from PyQt4 import QtCore, QtGui, QtWebKit
 import os
 
 class MyWebPage(QtWebKit.QWebPage):
+    """
+    a small webpage handling the navigation bar ??.
+
+    Why do we need this class?
+
+    """
+
     formSubmitted = QtCore.pyqtSignal(object)
 
     def acceptNavigationRequest(self, frame, req, nav_type):
@@ -17,15 +31,53 @@ class MyWebPage(QtWebKit.QWebPage):
                 value = unquote_plus(bytes(value)).decode('utf8')
                 elements[key] = value
             self.formSubmitted.emit(elements)
-        return super(MyWebPage, self).acceptNavigationRequest(frame, req, nav_type)
+        return super(MyWebPage, self).acceptNavigationRequest(frame, req,
+                nav_type)
 
 class PsyTML(QtGui.QDialog):
-    def __init__(self, size, position = True):
+    """
+    convenient object to show html files and collect the data out of a
+    submitted form.
+
+    Features
+    --------
+
+    * present html forms with JavaScript and css
+    * data that is submitted is collected in a Python dict
+    * multiple pages are possible
+
+    Limitations
+    -----------
+
+    * only local html files are supported at the moment
+    * only GET method is supported to transmit data to Python
+
+    Known Bugs
+    ----------
+
+    * sometimes runs into troubles, when non ascii letters are used in a text
+      field
+
+    """
+    def __init__(self, size, position=True):
+        """
+        Parameters
+        ----------
+
+        size : tuple
+            tuple containing the width and the height of the window in pixel
+        position : *None* or tuple
+            if None QtWebKit default position is used, otherwise the position
+            given in a tuple containing the left top pixel position of the
+            window.
+
+        """
         super(PsyTML, self).__init__()
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
         self.resize(size[0], size[1])
+
         self.position = position
-        if (self.position != True):
+        if not position is None:
             self.move(self.position[0], self.position[1])
         self.data = {}
         self.view = QtWebKit.QWebView(self)
@@ -36,23 +88,37 @@ class PsyTML(QtGui.QDialog):
         self.view.page().formSubmitted.connect(self.handleFormSubmitted)
 
     def viewPsyTML(self, html):
+        """
+        load a html file and present it on the screen.
+
+        """
         if os.path.isfile(html):
             page = "file:///" + os.path.abspath(html).replace("\\", "/")
             self.view.setUrl(QtCore.QUrl(page))
         else:
+            # TODO raise an exception here? (Zen of Python: Errors should never
+            # pass silently. Unless explicitly silenced.)
             print('Error, following html file not found:' + html)
             return True
-        if (self.position == True):
+        if self.position is None:
             self.center()
         self.exec_()
-    
+
     def center(self):
+        """
+        center the window on the screen.
+
+        """
         qr = self.frameGeometry()
         cp = QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
     def handleFormSubmitted(self, elements):
+        """
+        store the received data in self.data.
+
+        """
         self.data = elements
         self.accept()
 
@@ -68,25 +134,25 @@ class Start(QtGui.QDialog):
         font.setPointSize (14)
         self.setFont(font)
         self.initUI()
-    
-    
-    def initUI(self):               
+
+
+    def initUI(self):
         id_lab = QtGui.QLabel('Id (#): ')
         id_edit = QtGui.QLineEdit()
         id_edit.setMaxLength(5)
         id_edit.setText(str(self.id))
         id_edit.setReadOnly(True)
         okButton = QtGui.QPushButton("OK")
-        okButton.clicked.connect(self.close)       
+        okButton.clicked.connect(self.close)
         #cancelButton = QtGui.QPushButton("Cancel!")
-        #cancelButton.clicked.connect(QtCore.QCoreApplication.instance().quit)       
-        
+        #cancelButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+
         hbox = QtGui.QHBoxLayout()
         hbox.addWidget(id_lab)
         hbox.addWidget(id_edit)
         vBox = QtGui.QVBoxLayout()
         vBox.addLayout(hbox)
-        
+
         if (self.condition != False):
             hboxC = QtGui.QHBoxLayout()
             condition_lab = QtGui.QLabel('Condition: ')
@@ -96,19 +162,19 @@ class Start(QtGui.QDialog):
             hboxC.addWidget(condition_lab)
             hboxC.addWidget(condition_edit)
             vBox.addLayout(hboxC)
-        
+
         hbox2 = QtGui.QHBoxLayout()
         hbox2.addStretch(1)
         #hbox2.addWidget(cancelButton)
         hbox2.addWidget(okButton)
-        
+
         vBox.addLayout(hbox2)
         self.setLayout(vBox)
-        
-        self.setWindowTitle('Starting Experiment')    
+
+        self.setWindowTitle('Starting Experiment')
         self.center()
         self.exec_()
-        
+
     def center(self):
         qr = self.frameGeometry()
         cp = QtGui.QDesktopWidget().availableGeometry().center()
--- examples/psytml_example_multiple.py
+++ examples/psytml_example_multiple.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# last mod 2012-09-20 13:49 KS
 import sys
-import os
+sys.path.append("../") # include folder, where the PsyTML.py file is in
 import PsyTML
 from PyQt4 import QtGui
 
@@ -9,23 +12,23 @@ def main():
 
     intro = PsyTML.PsyTML((900, 400), True)
     intro.viewPsyTML("./sampleHTML/intro1.html")
-    
+
     item = PsyTML.PsyTML((900, 800), (0, 0))
-    item.viewPsyTML("./sampleHTML/item1.html")    
+    item.viewPsyTML("./sampleHTML/item1.html")
     results1 = item.data
     item.viewPsyTML("./sampleHTML/item2.html")
     results2 = item.data
-    
+
     # Note the demographics use jsVal (by Karl Seguin & Timo Haberkern) relaesed under LGPL
     # (and are in German-)
     demographics = PsyTML.PsyTML((900, 500), (200, 200))
-    demographics.viewPsyTML("./sampleHTML/demographics.html")    
+    demographics.viewPsyTML("./sampleHTML/demographics.html")
     demographic_data = demographics.data
-    
+
     print(results1)
     print(results2)
     print(demographic_data)
-    
+
 
 if __name__ == "__main__":
     main()
